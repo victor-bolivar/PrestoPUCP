97f2dc0b7b865aeddd9726dcd530ea979ba95175
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2022-11-11 07:34:13 -0500

    xml UsuarioTI_SolicitudesPendientes: antes de implementar scrollview

---
+++ app/src/main/java/com/example/prestopucp/UsuarioTI_SolicitudesPendientes.java
@@ -0,0 +1,14 @@
+package com.example.prestopucp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsuarioTI_SolicitudesPendientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_ti_solicitudes_pendientes);
+    }
+}
